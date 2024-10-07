c0edcab545f543d4c08ef62268432784024d9da6
Grzegorz Pustelnik <grzeniux28@gmail.com>
2024-10-07 12:58:39 +0200

    poprawka, zliczanie takze katalogow

--- Lab 1/zadanie1.py
+++ Lab 1/zadanie1.py
@@ -4,16 +4,19 @@
 
 import os
 
-def count_files_in_directory(directory):
+def count_files_and_directories(directory):
     try:
         items = os.listdir(directory)
         file_count = sum(1 for item in items if os.path.isfile(os.path.join(directory, item)))
-        return file_count
+        dir_count = sum(1 for item in items if os.path.isdir(os.path.join(directory, item)))
+        return file_count, dir_count
     except FileNotFoundError:
         return "Podany katalog nie istnieje."
     except PermissionError:
         return "Brak dostępu do podanego katalogu."
 
 directory_path = r'C:\Users\grzen\Desktop\Jazda\Notepads'
-file_count = count_files_in_directory(directory_path)
+file_count, dir_count = count_files_and_directories(directory_path)
+
 print(f"Ilość plików w katalogu {directory_path}: {file_count}")
+print(f"Ilość katalogów w katalogu {directory_path}: {dir_count}")
