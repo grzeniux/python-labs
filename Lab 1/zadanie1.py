679c30d317c63aa1aa9f8137c353965bdf7a0249
Grzegorz Pustelnik <grzeniux28@gmail.com>
2024-10-07 12:55:49 +0200

    rozw_zad1

--- Lab 1/zadanie1.py
+++ Lab 1/zadanie1.py
@@ -1,3 +1,19 @@
 # Zadanie 1
 # Napisz skrypt zliczający ilość plików w katalogu /dev (lub w dowolnym katalogu), 
 # skorzystaj ze standardowej biblioteki - os
+
+import os
+
+def count_files_in_directory(directory):
+    try:
+        items = os.listdir(directory)
+        file_count = sum(1 for item in items if os.path.isfile(os.path.join(directory, item)))
+        return file_count
+    except FileNotFoundError:
+        return "Podany katalog nie istnieje."
+    except PermissionError:
+        return "Brak dostępu do podanego katalogu."
+
+directory_path = r'C:\Users\grzen\Desktop\Jazda\Notepads'
+file_count = count_files_in_directory(directory_path)
+print(f"Ilość plików w katalogu {directory_path}: {file_count}")
