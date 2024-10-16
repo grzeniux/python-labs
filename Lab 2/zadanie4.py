26fb5ce71d02d0b21b2e930aabae0db8ab62f0a1
Grzegorz Pustelnik <grzeniux28@gmail.com>
2024-10-16 22:15:17 +0200

    zad4

--- Lab 2/zadanie4.py
+++ Lab 2/zadanie4.py
@@ -4,3 +4,34 @@
 # po których funkcja  __next__  rzuca wyjątek StopIteration
 
 
+class Fibonacci:
+    def __init__(self, steps):
+        self.steps = steps      # liczba wyrazów ciągu
+        self.current_step = 0   # aktualny krok
+        self.a, self.b = 0, 1   # pierwsze dwie liczby Fibonacciego
+
+    def __iter__(self):
+        """
+        Zwraca iterator (self), aby klasa była iterowalna.
+        """
+        return self
+
+    def __next__(self):
+        """
+        Zwraca kolejny wyraz ciągu Fibonacciego. Po osiągnięciu limitu kroków rzuca wyjątek StopIteration.
+        """
+        if self.current_step >= self.steps:
+            raise StopIteration
+        fib_number = self.a
+
+        self.a, self.b = self.b, self.a + self.b
+        self.current_step += 1
+        
+        return fib_number
+
+fibonacci_iterator = Fibonacci(steps=10)
+
+for num in fibonacci_iterator:
+    print(num)
+
+
