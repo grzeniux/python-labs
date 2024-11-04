f84f457d67093dfc9f3523cc9299ee0687ede3bd
Grzegorz Pustelnik <grzeniux28@gmail.com>
2024-11-04 12:44:08 +0100

    rozw

--- Lab 3/zad1.py
+++ Lab 3/zad1.py
@@ -1,2 +1,13 @@
 # Użyj słowa yield i zrób generator wielokrotności dowolnej liczby
 
+
+def multiple(liczba):
+    mnoznik = 1
+    while True:
+        yield liczba * mnoznik
+        mnoznik += 1
+
+gen = multiple(5)
+
+for x in range(10):
+    print(next(gen))
